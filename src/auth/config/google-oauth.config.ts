db305a167ac657f52657a81d3e4540dffe7a9cfd
unknown <youkata666@gmail.com>
2025-04-07 02:24:52 +0700

    rollback google secret concept

--- src/auth/config/google-oauth.config.ts
+++ src/auth/config/google-oauth.config.ts
@@ -1,15 +1,7 @@
 import { registerAs } from "@nestjs/config";
 
-const client = `303771527448-5kuvjfi325nus0tssimjvng2bdpic90p.apps.googleusercontent.com`;
-const secret = `303771527448-5kuvjfi325nus0tssimjvng2bdpic90p.apps.googleusercontent.com`;
-const cbu = `https://first-project-with-nest-production.up.railway.app/api/auth/google/callback`;
-console.log('[google-oauth.config.ts] Loaded ENV:', {
-    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-    GOOGLE_CALLBACK_URL: process.env.GOOGLE_CALLBACK_URL,
-})
 export default registerAs("googleOAuth", () => ({
-    clientID: client,
-    clientSecret: secret,
-    callbackURL: cbu,
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK_URL,
 }))
