cf36f41d42158502ea01704c357db49d04f02f9d
unknown <youkata666@gmail.com>
2025-04-06 17:40:17 +0700

    change configModule imported from

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { CommonModule } from './common/common.module';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import googleOauthConfig from './auth/config/google-oauth.config';
+import jwtConfig from './auth/config/jwt.config';
 
 
 @Module({
-  imports: [CommonModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Penting supaya config bisa diakses dari mana saja
+      load: [googleOauthConfig, jwtConfig], // Semua config dimuat di sini
+    }),
+    CommonModule,
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,21 +1,14 @@
 import { Module, forwardRef } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
-import jwtConfig from "./config/jwt.config";
 import { ConfigModule, ConfigService } from "@nestjs/config";
 import { PassportModule } from "@nestjs/passport";
 import { JwtStrategy } from "./jwt.strategy";
-import googleOauthConfig from "./config/google-oauth.config";
 import { GoogleStrategy } from "./google.strategy";
 import { UserModule } from '../user/user.module';
 
 @Module({
   imports: [
-    ConfigModule.forFeature(googleOauthConfig),
-
-    ConfigModule.forRoot({
-      load: [jwtConfig],
-    }),
-
+    ConfigModule,
     PassportModule,
 
     JwtModule.registerAsync({
